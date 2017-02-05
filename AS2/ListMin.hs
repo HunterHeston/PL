ad0e6ebeec6a360b47db3bd9cb06c9d2a7101420
Cody <wcodyyoung4@gmail.com>
2017-02-05 05:18:47 -0500

    fixing merge conflict on my end

--- AS2/ListMin.hs
+++ AS2/ListMin.hs
@@ -1,13 +1,6 @@
 module ListMin where
-<<<<<<< HEAD
 
-listMin :: (Ord a) => [a] -> a
-
-listMin (x:[]) = x 
-listMin (x:y:xs) = listMin((min x y : xs))
-=======
 listMin :: (Ord a) => [a] -> a
 
 listMin (a:[]) = a
 listMin lst = listMin ((min (head lst) (head (tail lst))) : (tail (tail lst)))
->>>>>>> e88d63122abefd95b37996d1b01052eb54b85914

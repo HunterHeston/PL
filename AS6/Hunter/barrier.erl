35034d57a423eb3500e6a19907c43333a405b609
Hunter <Hunter_Heston@yahoo.com>
2017-04-17 17:30:00 -0400

    barrier working

--- AS6/Hunter/barrier.erl
+++ AS6/Hunter/barrier.erl
@@ -1,5 +1,5 @@
 -module(barrier).
--export([start/1]).
+-export([start/1, server/3]).
 
 start(N) ->
     spawn(barrier, server, [N,N,[]]).
@@ -8,19 +8,30 @@ start(N) ->
 server(N, M, Ids) ->
    case M of
       0 -> 
-         helper(self(), N, Ids);
+         helper(self(), N, Ids),
+         receive
+            {Pid, how_many_running} ->
+               Pid!{self(), number_running_is, M},
+               server(N, M, Ids)
+         end;
       _ ->
          receive
             {Pid, done} -> 
                Pid!{self(), ok},
-               server(N, M-1, Pid++Ids);
+               server(N, M-1, [Pid|Ids]);
             {Pid, how_many_running} ->
                Pid!{self(), number_running_is, M},
                server(N, M, Ids)
-         end.
+         end
+   end.
 
-helper(SPid, N, []) ->
-   server(N, N, []);
-helper(SPid, [H|T]) ->
+
+   
+
+
+
+helper(_, N, []) ->
+   done;
+helper(SPid, N, [H|T]) ->
    H!{SPid, continue},
    helper(SPid, N, T).
